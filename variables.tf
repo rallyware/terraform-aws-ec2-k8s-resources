fb8bf3c4eb29a67cc06f238c96d75a4eecd59862
Volodymyr Syromiatnykov <vladimir.fess@gmail.com>
2024-02-06 14:12:46 -0500

    upd

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "family" {
   type        = string
-  default     = "t2"
+  default     = "t3"
   description = "The family of EC2 instance type"
 }
 
@@ -14,9 +14,9 @@ variable "resources_binding_percentage" {
   type = object(
     {
       cpu_lower    = optional(number, 70)
-      cpu_upper    = optional(number, 90)
+      cpu_upper    = optional(number, 95)
       memory_lower = optional(number, 70)
-      memory_upper = optional(number, 90)
+      memory_upper = optional(number, 95)
     }
   )
   default     = {}
